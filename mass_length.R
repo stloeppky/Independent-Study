87ac4abc6fddcd55b96d87bc2dd49d8f0c1b4fa4
stloeppky <94871407+stloeppky@users.noreply.github.com>
2022-10-13 20:11:01 -0700

    Add files via upload

--- mass_length.R
+++ mass_length.R
@@ -85,7 +85,7 @@ x.lim <- range(cap.2021$MASS) # range for x axis
 y.lim <- range(cap.2021$SVL)  # range for y axis
 
 # make plot
-plot(NA, xlim = x.lim, ylim = y.lim, xlab = "Mass (gram)", ylab = "Snoutt to Vent Length (cm)")
+plot(NA, xlim = x.lim, ylim = y.lim, xlab = "Mass (gram)", ylab = "Snout to Vent Length (cm)")
 
 # now plot using points females and then males
 points(female$MASS, female$SVL, col = "red", pch = 19)
@@ -109,33 +109,59 @@ abline(lm(male$SVL ~male$MASS), col = "purple")
 # trying in ggplot because its easier for me to use for some reason 
 library(ggplot2)
 
-ggplot(cap.2021, aes(x = MASS, y = SVL)) +
-  geom_point(aes(colour = SEX)) + 
+ggplot(cap.2021, aes(x = MASS, y = SVL, colour = SEX)) +
+  geom_point() + 
+  geom_smooth(se = FALSE) +
   xlab("Mass (g)") +             
   ylab("Snout to Vent Length (cm)") + 
   scale_color_discrete("Sex") + 
-  geom_smooth(se = FALSE) # this does it for both F & M but trying to see how it might be different 
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"))
 
 
-# trying box plot? 
+# this does it for both F & M but trying to see how it might be different 
+
+# I would like to make this a logarithmic line or something else but unsure how 
+
+
+# trying box plot 
 
 ggplot(cap.2021, aes(x=MASS, y=SVL, fill = SEX)) + geom_boxplot()
 
-# trying to bin some data 
+# Just two big plots - does not show the data well 
+
+# trying to bin some data so that I can see more detail 
 
 library(dplyr)
 
 cap.2021 <- cap.2021 %>% mutate(MASS_bin = cut(MASS, breaks=10))
 
+
 # making a boxplot with the binned data 
 
+head(cap.2021) 
 
-head(cap.2021) # MASS_
-ggplot(cap.2021, aes(x= MASS_bin, y = SVL, fill = SEX)) + geom_boxplot() +
-   theme(axis.text.x = element_text(angle = 90)) +
+ggplot(cap.2021, aes(x= MASS_bin, y = SVL, fill = SEX, colour = SEX)) + geom_boxplot() +
+  geom_smooth(se=FALSE, aes(group=1),) +
+  theme(axis.text.x = element_text(angle = 90)) +
+  ylim(0, 110) +
   xlab("Mass (g)") +             
   ylab("Snout to Vent Length (cm)") + 
-  scale_color_discrete("Sex")
+  scale_color_discrete("Sex") + 
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+       panel.background = element_blank(), axis.line = element_line(colour = "black"))
+# tried   geom_smooth(method = "lm", se=FALSE, color="black", aes(group=1)) but I dont like this line 
+# trying different method. Removed method = lm - shows a moving trendline which looks more accurate I think
+# I still want to put two different trendlines for female and male 
+#using Fill and Colour in the ggplot section created two different legends... which is interesting... 
+
+# making this plot again but with two different plots for male and female 
+ggplot(cap.2021, aes(x= female$MASS, y = female$SVL)) + 
+  geom_point()
+
+
+# went back and put color = SEX into ggplot portion of the script, and that made it apply to
+# both the points and the lines 
 
 
 
